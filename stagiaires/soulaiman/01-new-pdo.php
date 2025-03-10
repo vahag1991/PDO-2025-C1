2668c85afde6f18a7909b273a27b89f187a199cb
souli <chair.soulaiman.map@gmail.com>
2025-03-10 13:36:31 +0100

    01-pdo

--- stagiaires/soulaiman/01-new-pdo.php
+++ stagiaires/soulaiman/01-new-pdo.php
@@ -1,5 +1,5 @@
 <?php
-const DB_CONNECT_TYPE = "mysql";
+const CONNECT_TYPE = "mysql";
 const DB_CONNECT_HOST = "localhost";
 const DB_CONNECT_PORT = 3306;
 const DB_CONNECT_NAME = "pdo_c1";
@@ -9,7 +9,7 @@ const DB_CONNECT_PWD = "";
 try {
 
     $db = new PDO(
-        dsn: DB_CONNECT_TYPE . ":host=" . DB_CONNECT_HOST . ";dbname=" . DB_CONNECT_NAME . ";port=" . DB_CONNECT_PORT . ";charset=" . DB_CONNECT_CHARSET,
+        dsn: CONNECT_TYPE . ":host=" . DB_CONNECT_HOST . ";dbname=" . DB_CONNECT_NAME . ";port=" . DB_CONNECT_PORT . ";charset=" . DB_CONNECT_CHARSET,
         username: DB_CONNECT_USER,
         password: DB_CONNECT_PWD,
     );
