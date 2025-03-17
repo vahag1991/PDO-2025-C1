1f15d4d88e9ca37e620b1124546e951f8c02b5f3
agim <agim.coroli8016@gmail.com>
2025-03-17 16:22:11 +0100

    add exe1,2,3 + config

---
+++ stagiaires/agim/config_pdo_c1.php
@@ -0,0 +1,11 @@
+<?php
+
+# Création des constantes de connexion
+
+const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
+const DB_CONNECT_HOST = "localhost";
+const DB_CONNECT_PORT = 3306;
+const DB_CONNECT_NAME = "pdo_c1";
+const DB_CONNECT_CHARSET = "utf8";
+const DB_CONNECT_USER = "root";
+const DB_CONNECT_PWD = "";
