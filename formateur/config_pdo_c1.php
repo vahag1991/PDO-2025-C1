e4dfb825414233fb24edbd11ce462fd04f78dfba
WebDevCF2m <gitweb@cf2m.be>
2025-03-17 14:58:05 +0100

    try catch perso

--- formateur/config_pdo_c1.php
+++ formateur/config_pdo_c1.php
@@ -5,7 +5,7 @@
 const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
 const DB_CONNECT_HOST = "localhost";
 const DB_CONNECT_PORT = 3306;
-const DB_CONNECT_NAME = "pdo_c1";
+const DB_CONNECT_NAME = "pdo_c10";
 const DB_CONNECT_CHARSET = "utf8";
 const DB_CONNECT_USER = "root";
 const DB_CONNECT_PWD = "";
---
+++ formateur/exception.php
@@ -0,0 +1,21 @@
+<?php
+// âge au hasard entre 16 et 77 ans.
+$age = rand(16,19);
+// on divise de 0 à 5.
+$diviseAge = rand(0,5);
+try{
+    if($diviseAge === 0) {
+        // création erreur perso
+        throw new Exception("Age n'est pas divisible par 0", 10001);
+    }elseif($age<18){
+        // création autre erreur perso
+        throw new Exception("Trop jeune",10002);
+    }else{
+        echo '$age / $diviseAge = '."$age / $diviseAge =".($age / $diviseAge);
+    }
+}catch(Exception $e){
+    die( "Code :".$e->getCode()."<br>"
+        .'Message($e->getMessage()) :'." {$e->getMessage()} <br>")
+    ;
+}
+
