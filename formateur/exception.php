3e476a72cb4c3068a9781804a6edde2cc5bf0522
souli <chair.soulaiman.map@gmail.com>
2025-03-19 10:58:19 +0100

    exe5

--- formateur/exception.php
+++ formateur/exception.php
@@ -1,21 +1,21 @@
 <?php
 // âge au hasard entre 16 et 77 ans.
-$age = rand(16,19);
+$age = rand(16, 19);
 // on divise de 0 à 5.
-$diviseAge = rand(0,5);
-try{
-    if($diviseAge === 0) {
+$diviseAge = rand(0, 5);
+try {
+    if ($diviseAge === 0) {
         // création erreur perso
         throw new Exception("Age n'est pas divisible par 0", 10001);
-    }elseif($age<18){
+    } elseif ($age < 18) {
         // création autre erreur perso
-        throw new Exception("Trop jeune",10002);
-    }else{
-        echo '$age / $diviseAge = '."$age / $diviseAge =".($age / $diviseAge);
+        throw new Exception("Trop jeune", 10002);
+    } else {
+        echo '$age / $diviseAge = ' . "$age / $diviseAge =" . ($age / $diviseAge);
     }
-}catch(Exception $e){
-    die( "Code :".$e->getCode()."<br>"
-        .'Message($e->getMessage()) :'." {$e->getMessage()} <br>")
+} catch (Exception $e) {
+    die("Code :" . $e->getCode() . "<br>"
+        . 'Message($e->getMessage()) :' . " {$e->getMessage()} <br>")
     ;
 }
 
