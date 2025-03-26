36321cafe693808640cf8bd0514c6fa3d2c66b95
Nono1050 <abdelkadernordine@hotmail.com>
2025-03-26 13:28:00 +0100

    exo 6

---
+++ stagiaires/nordine/06-exercice-query-3-tables/view/accueil.view.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil de notre site</title>
+</head>
+<body>
+<?php
+include "inc/menu.inc.view.php";
+?>
+<h1>Accueil de notre site</h1>
+<p>Bienvenue sur notre site !</p>
+<p>Les 3 autres pages vont afficher du contenu d'une base de donnée, chacune affectant une table</p>
+<p>Faites un css perso et changez-moi cette photo</p>
+<img src="img/ia.jpg_large" alt="mikhawa" title="mikhawa"/>
+</body>
+</html>
