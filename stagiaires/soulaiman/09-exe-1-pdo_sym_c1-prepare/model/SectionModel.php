26f57427b2df5d8300a002806b3c2d1af170420c
souli <chair.soulaiman.map@gmail.com>
2025-04-01 09:21:07 +0200

    exe9

---
+++ stagiaires/soulaiman/09-exe-1-pdo_sym_c1-prepare/model/SectionModel.php
@@ -0,0 +1,28 @@
+<?php
+# MODEL
+# modèle qui gère les sections
+
+/**
+ * Fonction qui récupère toutes les sections
+ * uniquement les champs id, section_title et section_slug
+ * @param PDO $db
+ * @return array|string
+ */
+
+function getAllSectionsMenu(PDO $connectDB) : array|string
+{
+    // var_dump($pdo);
+    try {
+        $request = $connectDB->query("
+SELECT id, section_title, section_slug 
+FROM section");
+        // pas de résultat, on envoie l'erreur
+        if($request->rowCount() === 0) return "Pas encore de section";
+        // sinon (non visible, car return ligne précédente)
+        return $request->fetchAll();
+    }catch (Exception $e){
+        return $e->getMessage();
+    }
+}
+
+
