b181f9ec6918713f9a7fe013b0cad4949a1aae29
WebDevCF2m <gitweb@cf2m.be>
2025-04-01 10:31:36 +0200

    correct 09 michael good

--- stagiaires/michael/09-exe-1-pdo_sym_c1-prepare/view/public.homepage.view.php
+++ stagiaires/michael/09-exe-1-pdo_sym_c1-prepare/view/public.homepage.view.php
@@ -17,9 +17,26 @@
 <h3>Nos 10 derniers articles</h3>
 <p>Affichage des 10 derniers articles</p>
 <?php
+// pas de résultats ou erreur (donc en string)
+if(is_string($articles)):
+?>
+<h2><?=$articles?></h2>
+<?php
+// on a des résultats
+else:
+    // on les affiche lignes par lignes
+    foreach ($articles as $article):
+?>
+<h4><a href="?article=<?=$article['title_slug']?>"><?=$article['title']?></a></h4>
+<h5>Ecrit par ... le <?=$article['article_date_create']?></h5>
+<p><?=$article['text']?></p>
+<hr>
+<?php
+    endforeach;
+endif;
 //var_dump($pdo);
 //var_dump($menu);
-var_dump($articles);
+//var_dump($articles);
 ?>
 </body>
 </html>
