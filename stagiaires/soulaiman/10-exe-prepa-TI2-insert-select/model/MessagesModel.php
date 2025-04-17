db63ea46ed052ebb40e88b1ca370c6a8f5c4d897
souli <chair.soulaiman.map@gmail.com>
2025-04-17 13:19:45 +0200

    exe10b

---
+++ stagiaires/soulaiman/10-exe-prepa-TI2-insert-select/model/MessagesModel.php
@@ -0,0 +1,39 @@
+<?php
+
+function getAllarticleByDateDesc(PDO $con): array
+{
+    try {
+      return  $con->query("SELECT * FROM article ORDER BY `create_date` DESC;")->fetchAll();
+    }catch(Exception $e){
+        die("Code erreur : {$e->getCode()} | Message : {$e->getMessage()}");
+    }
+}
+
+function addNewarticle(PDO $con, string $name, string $email, string $message): bool|string
+{
+    $error="";
+    $nameInsert = trim(htmlspecialchars(strip_tags($name),));
+    $emailInsert = filter_var($email,FILTER_VALIDATE_EMAIL); # garde l'email si juste, envoie false si non correcte
+    $messageInsert = trim(htmlspecialchars(strip_tags($message),ENT_QUOTES));
+
+    // vérification des champs
+    if(empty($nameInsert)) $error .="Nom incorrecte<br>";
+    if(strlen($nameInsert)>60) $error .= "Nom trop long !<br>";
+    if($emailInsert===false) $error .="Email non valide";
+    if(empty($messageInsert)) $error .="Message incorrecte<br>";
+    if(strlen($messageInsert)>500) $error .= "Message trop long !<br>";
+
+    # si on a une erreur, on sort et on envoie les erreurs
+    if(!empty($error)) return $error;
+
+     try{
+           
+   $con->prepare("INSERT INTO `article`(`surname`,`email`,`message`)VALUES (?,?,?);")->execute([$nameInsert,$emailInsert,$messageInsert]);
+      
+        return true;
+    }catch (Exception $e){
+        die($e->getMessage());
+    }
+
+
+}
